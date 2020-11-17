8589ad86129040e9efc095eb4e34d4d1eeed92c1
caroline jasinski <jasinski_caroline@yahoo.fr>
2020-11-17 18:38:24 +0100

    wiki finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to articles_path
+      redirect_to article_path(@article)
       flash[:notice] = "article created"
     else
       render :new
